d72c543b36dc7cde60fabf409dec6e4e4f26ebf7
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2019-12-27 18:35:49 -0700

    static and templates

---
+++ static/js/app.js
@@ -0,0 +1,30 @@
+
+//----------------------------------------------------------------------------
+//function addItem(){
+        //var li = document.createElement("LI");  
+        //var input = document.getElementById("btnAdd");
+        //li.innerHTML = input.value;
+        //input.value = "";
+//
+        //document.getElementById("faves").appendChild(li);
+//}
+//
+//input.on("click", addItem());
+
+d3.selectAll("#btnAdd").on("click", function() {
+//  // What will be logged out? What is `this` in this case?
+  console.log(this.value);
+  var item = d3.select("ul").append("li");
+  item.text(this.value)
+//  // Answer: It will console log the `button` element.
+});
+//
+//
+//d3.selectAll("li").on("click", function() {
+//  // you can select the element just like any other selection
+//  var listItem = d3.select(this);
+//  listItem.style("color", "blue");
+//
+//  var listItemText = listItem.text();
+//  console.log(listItemText);
+//});
