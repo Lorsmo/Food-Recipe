1fc6795ce16ede4d67e9b69c15e690164d87f671
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2020-01-06 19:59:06 -0600

    loading button

--- static/js/app.js
+++ static/js/app.js
@@ -11,13 +11,13 @@
 //
 //input.on("click", addItem());
 
-d3.selectAll("#btnAdd").on("click", function() {
-//  // What will be logged out? What is `this` in this case?
-  console.log(this.value);
-  var item = d3.select("ul").append("li");
-  item.text(this.value)
-//  // Answer: It will console log the `button` element.
-});
+//d3.selectAll("#btnAdd").on("click", function() {
+////  // What will be logged out? What is `this` in this case?
+  //console.log(this.value);
+  //var item = d3.select("ul").append("li");
+  //item.text(this.value)
+////  // Answer: It will console log the `button` element.
+//});
 //
 //
 //d3.selectAll("li").on("click", function() {
@@ -28,3 +28,14 @@ d3.selectAll("#btnAdd").on("click", function() {
 //  var listItemText = listItem.text();
 //  console.log(listItemText);
 //});
+
+$(document).ready(function() {
+        $("#btnFetch").click(function() {
+          // disable button
+          $(this).prop("disabled", true);
+          // add spinner to button
+          $(this).html(
+            `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
+          );
+        });
+    });
